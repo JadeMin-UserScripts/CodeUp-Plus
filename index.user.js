5a1d2b596a24f08ed7080f93cc40d606d811c317
JadeMin <alt0000@klartnet.ml>
2023-03-22 19:44:42 +0900

    자동 업데이트 및 배포

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		CodeUp+
 // @description	CodeUp+
-// @version		0.1.3
+// @version		0.1.1
 // @icon		https://icons.duckduckgo.com/ip2/codeup.kr.ico
 // @updateURL	https://github.com/JadeMin-UserScripts/CodeUp-Plus/raw/dist/index.user.js
 // @downloadURL	https://github.com/JadeMin-UserScripts/CodeUp-Plus/raw/dist/index.user.js
